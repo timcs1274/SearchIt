d693089557d95faab16ba2633547d98aa56d9ddc
timcs <timcs1274@gmail.com>
2022-09-01 16:24:54 -0400

    add gql mutation and queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+mutation loginUser($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+    }
+  }
+}
+`;
