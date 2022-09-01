b9a1857826fb92617fb86c1b29903448540a99af
timcs <timcs1274@gmail.com>
2022-09-01 15:38:26 -0400

    add removebook mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -60,8 +60,18 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },
 
-        removeBook {
+        removeBook: async (parent, args, context) => {
+            if(context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: args.bookId } } },
+                { new: true }
+            );
 
+            return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
 };
