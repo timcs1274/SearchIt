2185da44c5366ba89a98cd1c4a925dfc641f691a
timcs <timcs1274@gmail.com>
2022-09-01 16:40:20 -0400

    add apolloclient to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,19 @@ import Navbar from './components/Navbar';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
 function App() {
   return (
     <ApolloProvider client={client}>
